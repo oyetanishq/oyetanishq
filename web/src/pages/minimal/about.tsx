abe69092b67f2a352d1e510be1c432036dd344b4
Tanishq Singh <hi@tanishqsingh.com>
2025-06-26 03:26:41 +0530

    web: about lines update

--- web/src/pages/minimal/about.tsx
+++ web/src/pages/minimal/about.tsx
@@ -18,13 +18,13 @@ export default function About() {
 
     return (
         <div className="w-full flex flex-1 flex-col items-start justify-start sm:pt-16 pt-3">
-            <div className="max-w-2xl flex-1 flex flex-col gap-6 justify-end sm:justify-start mb-14">
+            <div className="max-w-2xl flex-1 flex flex-col gap-6 justify-end sm:justify-start mb-12">
                 <p className="text-sm sm:text-[15px] about-paragraph">
                     <b>Tanishq Singh</b> is a sophomore student at <i>Indian Institute of Technology Varanasi</i>, he is a versatile developer skilled in full-stack development, ml/dl, and DevOps.
                 </p>
                 <p className="text-sm sm:text-[15px] about-paragraph">
-                    He has more than 3 years of experience in building robust web application, including automated deployment pipelines using github actions, and docker. He has recently started with machine & deep learning, He has actively participated in hackathons and
-                    contributed to open-source.
+                    He has more than 3 years of experience in building robust web application, including automated deployment pipelines using github actions, and docker. And more than 52K minutes in spotify wrapped last year, He has recently started with machine & deep learning,
+                    He has actively participated in hackathons and contributed to open-source.
                 </p>
                 <p className="text-sm sm:text-[15px] about-paragraph">Some technologies he is confident in react, typescript, node, golang, sql, docker, ci/cd workflows, cpp, and tailwindcss.</p>
             </div>
