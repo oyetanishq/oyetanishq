d42ced92055652327a5827e14284a521ebadc134
Tanishq Singh <hi@tanishqsingh.com>
2025-06-26 08:45:44 +0530

    web: contact page heading

--- web/src/pages/minimal/contact.tsx
+++ web/src/pages/minimal/contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
-        <div>
-            <div>contact</div>
+        <div className="w-full flex-1 flex flex-col items-start justify-start mt-6 gap-4">
+            <span className="text-xl sm:text-2xl uppercase">Contact</span>
         </div>
     );
 }
